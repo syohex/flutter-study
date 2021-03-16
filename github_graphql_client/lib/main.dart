ece8a1dcef0b084ccdde04dc90b3ba9e157be8b9
Shohei YOSHIDA <syohex@gmail.com>
2021-03-16 14:10:18 +0900

    init commit

---
+++ github_graphql_client/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'github_oauth_credentials.dart';
+import 'src/github_login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github GraphQL API Client',
+      theme: ThemeData(
+        primarySwatch:  Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Github GraphQL API Client'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(builder: (context, httpClient) {
+      return Scaffold(
+        appBar: AppBar(
+            title: Text(title)
+        ),
+        body: Center(
+          child: Text(
+            'You are logged in to Github!',
+          ),
+        ),
+      );
+    },
+    githubClientId: githubClientId,
+    githubClientSecret: githubClientSecret,
+    githubScopes: githubScopes,
+    );
+  }
+}
+
